465a309129658070d41e07d28f0624dfc5a2ca5e
Djibril-Kane <djibrilkanediallo04@gmail.com>
2025-02-12 22:17:22 +0000

    Mise à jour des modèles Laravel

--- app/Models/Historisation.php
+++ app/Models/Historisation.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Historisation extends Model
 {
     //
+    public $timestamps = false;
+    protected $fillable = [
+        "idHistorisation",
+        "idAdmin",
+        "adresseIp",
+        "dateHistorisation",
+        "clef"
+    ];
 }
--- app/Models/Parrain.php
+++ app/Models/Parrain.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Parrain extends Model
 {
     //
+    public $timestamps = false;
+    protected $fillable = [
+        "idParrain",
+        "numElecteur",
+        "numBureauVote",
+        "dateParrainage",
+        "numTel",
+        "adresseMail"
+    ];
 }
