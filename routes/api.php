3724680e5a975aa7c08c67e073f9627cc7de28ce
Djibril-Kane <djibrilkanediallo04@gmail.com>
2025-02-16 13:55:40 +0000

    Ajout de la gestion des candidats : contrôleur et routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\CandidatsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/candidats', CandidatsController::class);
+Route::get('verify/{numeroElecteur}',[CandidatsController::class, 'verify']);
+Route::post('register', [CandidatsController::class, 'verify']);
