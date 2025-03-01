f560f559ce6da591ad415b10381e3665e0cba0c6
Djibril-Kane <djibrilkanediallo04@gmail.com>
2025-03-01 19:12:09 +0000

    Modification du fichier auth.php pour l authentification

--- config/auth.php
+++ config/auth.php
@@ -70,10 +70,10 @@ return [
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+        'administrateurs' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Administrateur::class,
+        ],
     ],
 
     /*
