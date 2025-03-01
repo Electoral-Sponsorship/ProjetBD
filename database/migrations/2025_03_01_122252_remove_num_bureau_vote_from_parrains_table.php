7701d0f88babcdfef794a51e5c3da3e2f22b618f
Samuel Atohoun <samuelatohoun2@gmail.com>
2025-03-01 12:28:46 +0000

    Modif de la structure de la base de données

---
+++ database/migrations/2025_03_01_122252_remove_num_bureau_vote_from_parrains_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('parrains', function (Blueprint $table) {
+            //
+            $table->dropColumn('numBureauVote');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('parrains', function (Blueprint $table) {
+            // Réajoute la colonne 'numBureauVote' en cas de rollback
+            $table->string('numBureauVote')->nullable();
+        });
+    }
+};
