e273dc4dbe55fa190c681330da11c3e6d4c6aedf
Samuel Atohoun <samuelatohoun2@gmail.com>
2025-03-17 20:36:42 +0000

    cors.php test2

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://sunuparrainage.vercel.app/'],
+    'allowed_origins' => ['https://sunuparrainage.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
